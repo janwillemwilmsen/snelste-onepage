edd219faa07b057806a81535adced3252fd56ecd
Jan-Willem Wilmsen <janwillemwilmsen@gmail.com>
2021-08-25 19:54:51 +0200

    ChangeJS to one-hour

--- _data/sites/ssg.js
+++ _data/sites/ssg.js
@@ -5,7 +5,7 @@ module.exports = {
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: false, // !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 23, // 24 hours
+		frequency: 60 * 1, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
